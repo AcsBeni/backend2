ede577c79e9f4f82845baf4bbfd4b880bb46182b
Acsbeni <acsbenjamin@turr.hu>
2025-09-09 10:33:42 +0200

    Szerver befejezés

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-let users =[{id: 1,name: "Béla", age: 22,gender: "male" },
-  {id:2,name: "Éva", age: 18, gender: "female"},
-  {id:3,name: "Tamás", age: 45, gender: "male"},
-  
- 
-  
-]
+//middleware
+app.use(express.json()); //json formátum megkövetelése
+app.use(express.urlencoded({ extended: true })); // req.body használata
 
+
+let users =[]
+const USERS_FILE = path.join(__dirname,'users.json');
+loadusers();
 
 app.get("/",(req, res) => {
  
@@ -17,7 +19,7 @@ app.get("/users",(req, res) => {
  
   res.send(users );
 });
-
+//http://127.0.0.1:3000/users/1
 
 app.get("/users/:id",(req, res) => {
   let id = req.params.id;
@@ -29,7 +31,76 @@ app.get("/users/:id",(req, res) => {
   return res.send("Nincs ilyen id-jű user" );
 
 });
+//Rest client hf
+//post new user
 
+app.post("/users",(req, res) => {
+  let data = req.body;
+  data.id = getnextid();
+  users.push(data);
+  res.send(users);
+  saveusers();
+});
+
+//update user by id
+app.patch("/users/:id",(req, res) => {
+  let id = req.params.id;
+  let data = req.body;
+  let idx = users.findIndex(user => user.id == id);
+  if (idx>-1) {
+    users[idx] = data;
+    users[idx].id = Number(id);
+    return res.send('A felhasználó adatai frissítve lettek' );
+  }
+  return res.send("Nincs ilyen id-jű user" );
+});
+
+
+
+//delete user by id
+app.delete("/users/:id",(req, res) => {
+  let id = req.params.id;
+  let idx = users.findIndex(user => user.id == id);  
+  if (idx>-1) {
+    users.splice(idx,1);
+    saveusers();
+    return res.send(users);
+  }
+})
 
 
 app.listen(3000)
+//other functions
+function getnextid() {
+  let nextid = 1;
+  if(users.length>0) {
+    return nextid;
+  }
+  let maxid = 0;
+  for (let i=0; i<users.length; i++) {
+    if(users[i].id>users[maxid].id) {
+      maxid = i;
+    }
+    
+  }
+  return users[maxid].id+1;
+}
+
+function loadusers() {
+  if(fs.existsSync(USERS_FILE)) {
+    const raw = fs.readFileSync(USERS_FILE);
+    try {
+      users = JSON.parse(raw);
+    }
+    catch(err) {
+      console.error(err);
+      users = [];
+    }
+  }
+  else {
+    saveusers();
+  }
+}
+function saveusers() {
+  fs.writeFileSync(USERS_FILE,JSON.stringify(users));
+}
